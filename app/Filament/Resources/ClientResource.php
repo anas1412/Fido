c3811ae36e9c88ce9cfb0a2dd8052e53dd10e429
anas1412 <anas.bassoumi@gmail.com>
2025-10-07 10:46:03 +0100

    fixed search

--- app/Filament/Resources/ClientResource.php
+++ app/Filament/Resources/ClientResource.php
@@ -79,6 +79,10 @@ class ClientResource extends Resource
         return ClientResource::getUrl('view', ['record' => $record]);
     }
 
+    public static function getGlobalSearchResultTitle(Model $record): string | Htmlable
+    {
+        return $record->name;
+    }
 
     public static function getGloballySearchableAttributes(): array
     {
